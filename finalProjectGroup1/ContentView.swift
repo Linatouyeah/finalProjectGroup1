057272b8c9900565e4f9337c631bf9356028667f
nawsheen <nawsheen.azad06@gmail.com>
2024-07-26 14:05:08 +0100

    i changed it

--- finalProjectGroup1/ContentView.swift
+++ finalProjectGroup1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("GROUP 1!")
+            Text("helloooo how you doing 😜")
         }
         .padding()
     }
