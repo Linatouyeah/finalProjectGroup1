153dce8fc1657cd087ceb3dfa195c1812ef2c49b
Lina <Linatouyeah233@gmail.com>
2024-07-26 13:53:41 +0100

    Initial Commit

---
+++ finalProjectGroup1/finalProjectGroup1App.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectGroup1App.swift
+//  finalProjectGroup1
+//
+//  Created by Scholar on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectGroup1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
