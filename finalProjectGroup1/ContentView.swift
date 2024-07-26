dd55c5297d00a49783aa3807b59de1989b382e70
Dorsa <dorsa.nasimi22@gmail.com>
2024-07-26 14:17:49 +0100

    hiii byeeeee

--- finalProjectGroup1/ContentView.swift
+++ finalProjectGroup1/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("helloooo how you doing 😜")
+            
+            //hiii byeee
         }
         .padding()
     }
